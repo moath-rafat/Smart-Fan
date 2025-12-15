b0dd9bde459b477bac6788f4ac37577d2c5eca8d
Moath <moathrafat10@gmail.com>
2025-12-15 18:15:41 +0300

    Implemented pwm controller task and fixed other bugs

--- Core/Src/pwm-controller-task.c
+++ Core/Src/pwm-controller-task.c
@@ -1,5 +1,22 @@
 #include "pwm-controller-task.h"
 
-void pwm_controller_handler(void * const pvParameters) {
+extern volatile uint8_t temp;
 
+void pwm_controller_handler(void * const pvParameters) {
+	uint8_t t = temp; // temp might change if an interrupt fires
+
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	for(;;) {
+		if(t > 40) {
+			// 100%
+			TIM2->CCR2 = TIM2->ARR;
+		}
+		else if(t >= 20 && t <= 40) {
+			// 75%
+			TIM2->CCR2 = (TIM2->ARR * 3) / 4;
+		} else {
+			TIM2->CCR2 = 0;
+		}
+		vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
+	}
 }
